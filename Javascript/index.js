348a77ae910b8ac8ffb06d71de04634066bf4f48
23Soap <19yusufozcan@gmail.com>
2024-05-07 15:33:41 -0400

    Update

---
+++ Javascript/index.js
@@ -0,0 +1,2 @@
+document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myP").textContent = "I like Pizza";
---
+++ Javascript/variable.js/variable.js
@@ -0,0 +1,37 @@
+// let age = 25;
+// let price = 10.99;
+// let gpa = 2.1;
+
+// console.log(typeof gpa);
+// console.log("You are ${age} years old");
+// console.log("The price is ${price}");
+// console.log("Your GPA is: ${gpa}");
+
+//variables
+
+// let firstName = "Soap";
+// let favoriteFood = "Lazagna";
+// let email = "Soap123@gmail.com";
+
+// console.log(typeof firstName);
+// console.log(`your name is ${firstName}`);
+// console.log(`you like ${favoriteFood}`);
+// console.log(`Your E-Mail is ${email}`);
+
+//Booleans
+
+// let online = false;
+// let forSale = true;
+// let isStudent = true;
+
+// console.log(`Enrolled: ${isStudent}`);
+// console.log(`Is this car for sale: ${forSale}`);
+// console.log(`Bro is online: ${online}`);
+
+let fullName = "Soap";
+let age = 22;
+let student = true;
+
+document.getElementById("p1").textContent = `your name is ${fullName}`;
+document.getElementById("p2").textContent = `Your age is: ${age}`;
+document.getElementById("p3").textContent = `Enrolled: ${student}`;
